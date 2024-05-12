86fcf8a28c7c215deb31a6e8cd29a9f99bf6b7cb
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-05-12 23:36:07 +0530

    Update wsgi.py

--- todo_site/wsgi.py
+++ todo_site/wsgi.py
@@ -8,10 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+project_home='/home/Sweta31/todo_site_'
+if project_home not in sys.path:
+    sys.path.insert(0,project_home)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_site.settings')
+os.environ['DJANGO_SETTINGS_MODULE']= 'deploy_on_pythonanywhere.settings'
 
 application = get_wsgi_application()
 
